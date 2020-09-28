c8af9d7f0252416890b23e3a9b25e68dcbaf4542
Hector A. Reyes <hreyes@amwtexas.com>
2020-09-28 00:23:58 -0600

    Fixed issue where program would get stuck in an infinite loop recieving information from client

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -11,14 +11,16 @@ def main():
     fileName = receive(conn)
     found = os.path.exists(fileName)
 
-    if found:
+    if found: #need to fix appending copy to the name of the file
         print("A file with that name already exists, making copy")
         Contents = receive(conn)
-        newFile = open(fileName+"-copy",'x')
+        name,ext = re.split('.',fileName,1)
+        name = name+"_copy"
+        newFile = open(name+ext,'x')
         newFile.write(Contents)
         newFile.close()
     else:
-        COntends = receive(conn)
+        Contents = receive(conn)
         newFile = open(fileName,'x')
         newFile.write(Contents)
         newFile.close()
@@ -29,17 +31,11 @@ def main():
 def receive(conn):
     received = conn.recv(64).decode()
     size,msg = re.split(':',received,1)
-    size = int(size)
+    size = int(size) - 1
 
-    print(size)
-    print(msg)
-    print(len(msg))
     while len(msg) < size:
         msg = msg + conn.recv(64).decode()
-        print(msg)
-        time.sleep(1)
         
-    print("done recieving message")
     return msg
 
 if __name__ == '__main__':
